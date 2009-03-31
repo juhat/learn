5b9ddb038be54be85528ef3241bf99a5869670d3
Attila Juhász <juhat@goraffe.hu>
2009-03-31 10:40:03 +0200

    Partially working again.

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090103144612) do
+ActiveRecord::Schema.define(:version => 20090110200643) do
+
+  create_table "posts", :force => true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.boolean  "published"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "resource_urls", :force => true do |t|
     t.string   "key"
