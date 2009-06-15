814a67aa96e2450c7a6af4ac0ada11c56d2352f7
Attila Juhász <juhat@goraffe.hu>
2009-06-15 14:38:40 +0200

    Removed god plugin.

--- vendor/plugins/moonshine_god/moonshine/init.rb
+++
@@ -1,3 +0,0 @@
-require "#{File.dirname(__FILE__)}/../lib/god.rb"
-
-include God
--- vendor/plugins/moonshine_god/recipes/god.rb
+++
@@ -1,7 +0,0 @@
-namespace :god do
-  desc "Reload god configuration."
-  task :reload do
-    sudo 'god load /etc/god/god.conf'
-  end
-end
-after 'deploy:restart', 'god:reload'
