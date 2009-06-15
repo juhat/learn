3d091a6af9ce87d32d38ee121c75af9aed46bb11
Attila Juhász <juhat@goraffe.hu>
2009-06-15 13:12:29 +0200

    Initial data moved to fixtures.

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090804142735) do
+ActiveRecord::Schema.define(:version => 20090604140343) do
 
   create_table "courses", :force => true do |t|
     t.string   "name"
---
+++ vendor/plugins/moonshine_god/lib/god.rb
@@ -0,0 +1,43 @@
+module God
+
+  # A recipe to install and configure god. 
+  # Put configuration files in config/god/*.god in your application.
+  def god(options = {})
+    gem 'god'
+
+    # tells god where to find the main config file
+    file '/etc/default/god',
+         :require => package('god'),
+         :content => "GOD_CONFIG=/etc/god/god.conf"
+
+    file '/etc/god', :ensure => :directory
+
+    # tells god to load all of the /etc/god/APPNAME.god
+    file '/etc/god/god.conf',
+         :require => file('/etc/god'),
+         :backup => false,
+         :notify => exec('restart_god'),
+         :content => template("#{File.dirname(__FILE__)}/../templates/god.conf.erb", binding)
+
+    # tells god to load all of the watches for this application
+    file "/etc/god/#{configuration[:application]}.god",
+        :require => file('/etc/god/god.conf'),
+        :content => "God.load '#{configuration[:deploy_to]}/current/config/god/*.god'",
+        :notify => exec('restart_god')
+
+    # upstart- start god at boot, respawn when necessary
+    file '/etc/event.d/god',
+        :content => File.read("#{File.dirname(__FILE__)}/../templates/god.upstart"),
+        :notify => exec('restart_god')
+
+    exec 'restart_god',
+        :command => 'stop god || true && start god',
+        :require => file('/etc/event.d/god'),
+        :refreshonly => true
+
+    logrotate '/var/log/god.log',
+        :options => ['daily','rotate 7','compress','missingok','sharedscripts'],
+        :postrotate => '/usr/bin/god quit > /dev/null' # will be restarted by upstart
+  end
+  
+end
