ee8af2e64da177bdc5082e09ff9096d6d6035ca9
Attila Juhász <juhat@goraffe.hu>
2009-06-16 21:38:09 +0200

    Changed smtp_tls plugin, hope resolve bugs.

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,4 +1,14 @@
-require "smtp_tls"
-mailer_config = File.open("#{RAILS_ROOT}/config/mailer.yml")
-mailer_options = YAML.load(mailer_config)
-ActionMailer::Base.smtp_settings = mailer_options
+# require "smtp_tls"
+# mailer_config = File.open("#{RAILS_ROOT}/config/mailer.yml")
+# mailer_options = YAML.load(mailer_config)
+# ActionMailer::Base.smtp_settings = mailer_options
+
+ActionMailer::Base.smtp_settings = {
+    :tls => true,
+    :address => "smtp.gmail.com",
+    :port => "587",
+    :domain => "atti.la",
+    :authentication => :plain,
+    :user_name => "learn",
+    :password => "webtools" 
+  }
