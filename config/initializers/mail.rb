1703a18ee677de7f1c8bab05d11596e8b90da886
Attila Juhász <juhat@goraffe.hu>
2009-06-16 21:44:12 +0200

    Fixes.

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -9,6 +9,6 @@ ActionMailer::Base.smtp_settings = {
     :port => "587",
     :domain => "atti.la",
     :authentication => :plain,
-    :user_name => "learn",
+    :user_name => "learn@atti.la",
     :password => "webtools" 
   }
